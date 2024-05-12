b97cfce4fdafcae88c5991223124121a0786a933
lixoten <lixoten@gmail.com>
2024-05-12 12:49:32 -0700

    userSolution and expected code are now displayed on screen Fix: Objects are now allowed to be uses as results Fix: several things codemirror default settings

--- docs/todo.js
+++ docs/todo.js
@@ -1,7 +1,7 @@
 
 
 
-// Todo 1
+// Todo 1  //
 // for a lack of better work.. prevent cheating
 // if a problem is about a key-work/method... make sure it is used in the checked code.
 const x = " let largest = numbers[0]; for (let i = 1; i < numbers.length; i++) { if (numbers[i] > largest) { largest = numbers[i]; } } return largest;";
